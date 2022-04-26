ad90c28b644f6a200d4725fe4b96fd696625b4c8
Artur Serra <artur_seilah@hotmail.com>
2022-04-26 13:21:44 +0400

    Created the import of the globe icon

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 import { ChannelList, useChatContext } from "stream-chat-react";
-// import Cookies
+// Import Cookies
 import Cookies from "universal-cookie";
 
+// Import Icons
+import GlobeIcon from "../assets/globe.png"
+
+// Import Components
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
+
+
+// Create Sidebar to display channels
+const Sidebar = () => {
+    <div className="channel-list__sidebar">
+        <div className="channel-list__sidebar__icon1">
+            <div className="icon1__inner">
+                <img src={GlobeIcon} alt="Countries" width="30"/>
+            </div>
+        </div>
+    </div>
+}
 
 const ChannelListContainer = () => {
     return (
