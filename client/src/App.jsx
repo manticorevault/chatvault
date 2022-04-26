1bc8f323a8491f3724608f3114311ef75c7774fb
Artur Serra <artur_seilah@hotmail.com>
2022-04-26 12:28:02 +0400

    Prepared the initial client folder for the project

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div>
+            <h1>
+                This is the NOMAD VAULT
+            </h1>
+        </div>
+    );
+}
+
+export default App;
