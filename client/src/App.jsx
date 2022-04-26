519f5c0f7b8090074f17aa54f713803c470248be
Artur Serra <artur_seilah@hotmail.com>
2022-04-26 12:36:17 +0400

    Created the basic structure for the Stream App in the App.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+
+require('dotenv').config()
+
+const apiKey = process.env.STREAM_API_KEY
+
+// Create an instance of a Stream Chat
+const client = StreamChat.getInstance(apiKey)
 
 const App = () => {
     return (
-        <div>
-            <h1>
-                This is the NOMAD VAULT
-            </h1>
+        <div className="app__wrapper">
+            <Chat client={ client } theme="team dark">
+                <ChannelListContainer 
+                
+                />
+
+                <ChannelContainer 
+                
+                />
+            </Chat>
         </div>
     );
 }
