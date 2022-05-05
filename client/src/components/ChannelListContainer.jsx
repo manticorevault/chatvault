8c5004f500f7b998436a5f319ed4305ba6674926
Artur Serra <artur_seilah@hotmail.com>
2022-05-05 11:33:39 +0400

    Updated the ChannelListContainer info and added the ChannelSearch component

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -3,29 +3,48 @@ import { ChannelList, useChatContext } from "stream-chat-react";
 // Import Cookies
 import Cookies from "universal-cookie";
 
-// Import Icons
-import GlobeIcon from "../assets/globe.png"
-
 // Import Components
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
 
+// Import Icons
+import GlobeIcon from "../assets/planet.png"
+import LogoutIcon from "../assets/logout.png"
 
-// Create Sidebar to display channels
-const Sidebar = () => {
+// Create Sidebar Functional Component
+// with instant return to display channels
+const Sidebar = () => (
     <div className="channel-list__sidebar">
         <div className="channel-list__sidebar__icon1">
             <div className="icon1__inner">
                 <img src={GlobeIcon} alt="Countries" width="30"/>
             </div>
         </div>
+
+        <div className="channel-list__sidebar__icon2">
+            <div className="icon1__inner">
+                <img src={LogoutIcon} alt="Logout" width="30"/>
+            </div>
+        </div>
     </div>
-}
+)
+
+const LogoHeader = () => (
+    <div className="channel-list__header">
+        <p className="chanel-list__header__text">
+            Nomad Vault
+        </p>
+    </div>
+)
+
 
 const ChannelListContainer = () => {
     return (
-        <div>
-            ChannelListContainer
-        </div>
+        <>
+            <Sidebar />
+            <div className="channel-list__list__wrapper">
+                <LogoHeader />
+            </div>
+        </>
     )
 }
 
--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,3 +1,4 @@
-// Create an export index for all containers
+// Create an export index for all components
 export { default as ChannelContainer } from "./ChannelContainer"
 export { default as ChannelListContainer } from "./ChannelListContainer"
+export { default as ChannelSearch } from "./ChannelSearch"
